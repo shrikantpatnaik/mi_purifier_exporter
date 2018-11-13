30cfbd7301eb4e8f0df4273fa9cfb1278c30d59b
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2018-11-13 08:17:10 +0800

    Add some error handling

--- exporter.py
+++ exporter.py
@@ -2,7 +2,7 @@ import time
 import json
 import sys
 
-from miio import airpurifier
+from miio import airpurifier, exceptions
 
 # noinspection PyProtectedMember
 from prometheus_client import start_http_server, Gauge, Info
@@ -18,10 +18,19 @@ filter_life_remaining = Gauge('mi_purifier_filter_life', 'Filter life in percent
 mode = Info('mi_purifier_status', 'Operational Mode of Purifier', ['name'])
 
 
+def exit_with_error(error):
+    sys.exit(error)
+
+
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        exit_with_error("JSON file must be passed as first argument")
 
     with open(sys.argv[1]) as f:
         purifiers = json.load(f)
+
+    if len(purifiers["purifiers"]) < 1:
+        exit_with_error("No purifiers found in JSON File")
 
     for purifier in purifiers["purifiers"]:
         purifier["object"] = airpurifier.AirPurifier(ip=purifier["ip"], token=purifier["token"])
@@ -31,12 +40,17 @@ if __name__ == '__main__':
     while True:
         time.sleep(1)
         for purifier in purifiers["purifiers"]:
-            status = purifier["object"].status()
-            aqi.labels(purifier["name"]).set(status.aqi)
-            temp.labels(purifier["name"]).set(status.temperature)
-            humidity.labels(purifier["name"]).set(status.humidity)
-            filter_life_remaining.labels(purifier["name"]).set(status.filter_life_remaining)
-            mode.labels(purifier["name"]).info({
-                'mode': status.mode.name,
-                'filterType': status.filter_type.name
-            })
+            try:
+                status = purifier["object"].status()
+                aqi.labels(purifier["name"]).set(status.aqi)
+                temp.labels(purifier["name"]).set(status.temperature)
+                humidity.labels(purifier["name"]).set(status.humidity)
+                filter_life_remaining.labels(purifier["name"]).set(status.filter_life_remaining)
+                mode.labels(purifier["name"]).info({
+                    'mode': status.mode.name,
+                    'filterType': status.filter_type.name
+                })
+            except exceptions.DeviceException as error:
+                pass
+            except OSError as error:
+                pass
